494cbe0547a1e97b010461f6ad21987dc9704dc6
mariaclaravfa <143226419+mariaclaravfa@users.noreply.github.com>
2025-10-27 18:46:16 +0000

    .

--- db/DML.sql
+++ db/DML.sql
@@ -3,9 +3,9 @@ INSERT INTO Administrador (usuario, senha) VALUES
 ('editor.conteudo', 'hash_eC002');
 
 INSERT INTO Produto (nome, categoria, descricao, preco, imagem, cores, id_admin) VALUES
-('Jogo de Cama Percal 400 Fios', 'Cama e Banho', 'Conjunto de lençóis e fronhas em percal egípcio, toque acetinado.', 450.99, NULL, 'Branco, Azul Marinho, Cinza Chumbo', 1),
-('Toalha de Banho Gigante Algodão Pima', 'Cama e Banho', 'Toalha de banho com alta absorção e gramatura premium.', 95.50, NULL, 'Bege, Verde Musgo', 2),
-('Cortina Blackout em Voil', 'Decoração', 'Cortina com camada blackout e acabamento em voil, 2.80m x 2.30m.', 289.00, NULL, 'Gelo, Palha', 1);
+('Jogo de Cama Percal 400 Fios', 'cama, mesa e banho', 'Conjunto de lençóis e fronhas em percal egípcio, toque acetinado.', 450.99, NULL, 'Branco, Azul Marinho, Cinza Chumbo', 1),
+('Toalha de Banho Gigante Algodão Pima', 'cama, mesa e banho', 'Toalha de banho com alta absorção e gramatura premium.', 95.50, NULL, 'Bege, Verde Musgo', 2),
+('Cortina Blackout em Voil', 'cama, mesa e banho', 'Cortina com camada blackout e acabamento em voil, 2.80m x 2.30m.', 289.00, NULL, 'Gelo, Palha', 1);
 
 INSERT INTO BannerRotativo (id_banner, imagens, id_admin) VALUES
 (
